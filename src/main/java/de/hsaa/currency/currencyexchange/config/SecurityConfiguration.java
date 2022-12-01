bf2c013e3a8d84b588d373f1fbed24c6b914c62b
ufuk <altin.ufuk@gmail.com>
2022-12-01 21:02:44 +0100

    initial commit

--- src/main/java/de/hsaa/currency/currencyexchange/config/SecurityConfiguration.java
+++ src/main/java/de/hsaa/currency/currencyexchange/config/SecurityConfiguration.java
@@ -1,2 +1,25 @@
-package de.hsaa.currency.currencyexchange.config;public class SecurityConfiguration {
+package de.hsaa.currency.currencyexchange.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests()
+                .anyRequest().permitAll();
+
+        httpSecurity.csrf()
+                .disable();
+        httpSecurity.headers()
+                .frameOptions()
+                .sameOrigin();
+        httpSecurity.cors();
+        return httpSecurity.build();
+    }
 }
--- src/main/java/de/hsaa/currency/currencyexchange/controller/HelloWorld.java
+++ src/main/java/de/hsaa/currency/currencyexchange/controller/HelloWorld.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/exchange")
-public class ExchangeController {
+@RequestMapping("/hello")
+public class HelloWorld {
 
     @GetMapping()
     public ResponseEntity<String> getExchange() {
